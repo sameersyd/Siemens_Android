b8985ce608e4c2fa718e73dbdfaeefaf31cce4f2
Sameer Nawaz <sameer.nwaz@gmail.com>
2019-08-16 16:16:41 +0530

    Peermission/loading/disable_btns

--- app/src/main/java/com/sameer/siemens/MainActivity.java
+++ app/src/main/java/com/sameer/siemens/MainActivity.java
@@ -1,16 +1,21 @@
 package com.sameer.siemens;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -33,17 +38,21 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.Files;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements ImagePickerCallback {
 
+    Document document;
     EditText agreeNumEdit,custNameEdit,addressEdit;
     Button generatePdfBtn;
     TextView selectTxt;
     private ImagePicker imagePicker;
     List<ChosenImage> imgList = null;
     public static final String DEST = Environment.getExternalStorageDirectory().getPath() + "/siemensPDF/"+"Siemens.pdf";
+
+    View alpView;
+    ProgressBar pg;
+    public static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,6 +64,8 @@ public class MainActivity extends AppCompatActivity implements ImagePickerCallba
         addressEdit = (EditText)findViewById(R.id.main_address);
         selectTxt = (TextView) findViewById(R.id.main_selectImgTxt);
         generatePdfBtn = (Button) findViewById(R.id.main_generatePdfBtn);
+        alpView = (View)findViewById(R.id.main_view);
+        pg = (ProgressBar)findViewById(R.id.main_progress);
 
         selectTxt.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -62,10 +73,12 @@ public class MainActivity extends AppCompatActivity implements ImagePickerCallba
                 openImagePick();
             }
         });
+
         generatePdfBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                generatePdfMethod();
+                if (isStoragePermissionGranted())
+                    generatePdfMethod();
             }
         });
 
@@ -88,7 +101,9 @@ public class MainActivity extends AppCompatActivity implements ImagePickerCallba
 
         try {
 
-            Document document = new Document(PageSize.A4);
+            showHUD();
+
+            document = new Document(PageSize.A4);
 
             String root = Environment.getExternalStorageDirectory().toString();
 
@@ -103,12 +118,21 @@ public class MainActivity extends AppCompatActivity implements ImagePickerCallba
             document.close();
 
             Toast.makeText(this, "PDF Generated", Toast.LENGTH_SHORT).show();
+            agreeNumEdit.setText("");
+            custNameEdit.setText("");
+            addressEdit.setText("");
+            imgList = null;
+
+            hideHUD();
 
         } catch (DocumentException e) {
-            e.printStackTrace();
+            hideHUD();
+            Toast.makeText(this, e+"", Toast.LENGTH_SHORT).show();
         } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            hideHUD();
+            Toast.makeText(this, e+"", Toast.LENGTH_SHORT).show();
         }catch (Exception e){
+            hideHUD();
             Toast.makeText(this, e+"", Toast.LENGTH_SHORT).show();
         }
 
@@ -137,7 +161,6 @@ public class MainActivity extends AppCompatActivity implements ImagePickerCallba
                 try {
                     uploadImage(originalFileUri.getPath());
                 } catch (Exception ex) {
-                    Log.v("******",ex+"");
                     Toast.makeText(this, ex+"", Toast.LENGTH_SHORT).show();
                 }
             }
@@ -179,22 +202,15 @@ public class MainActivity extends AppCompatActivity implements ImagePickerCallba
 
     public void uploadImage(String filePath){
         try{
+            InputStream ims = new FileInputStream(filePath);
+            Bitmap bimp = BitmapFactory.decodeStream(ims);
+            ByteArrayOutputStream strm = new ByteArrayOutputStream();
+            bimp.compress(Bitmap.CompressFormat.PNG, 100, strm);
+            Image image1 = Image.getInstance(strm.toByteArray());
+            image1.scaleToFit(150, 150);
+            image1.setSpacingAfter(30f);
 
-//            InputStream ims = new FileInputStream(filePath);
-//            Bitmap bimp = BitmapFactory.decodeStream(ims);
-//            ByteArrayOutputStream strm = new ByteArrayOutputStream();
-//            bimp.compress(Bitmap.CompressFormat.PNG, 100, strm);
-//            Image image1 = Image.getInstance(strm.toByteArray());
-//            image1.scaleToFit(150, 150);
-//            image1.setSpacingAfter(30f);
-
-            InputStream img = getAssets().open("pre.png");
-
-            Bitmap bmp = BitmapFactory.decodeStream(img);
-            ByteArrayOutputStream stream = new ByteArrayOutputStream();
-            bmp.compress(Bitmap.CompressFormat.PNG, 100, stream);
-            Image image = Image.getInstance(stream.toByteArray());
-            image.scaleToFit(150, 150);
+            document.add(image1);
 
         }catch (Exception e){
             Toast.makeText(this, e+"", Toast.LENGTH_SHORT).show();
@@ -206,4 +222,46 @@ public class MainActivity extends AppCompatActivity implements ImagePickerCallba
     public void onError(String s) {
         Toast.makeText(this, s+"", Toast.LENGTH_SHORT).show();
     }
+
+    public  boolean isStoragePermissionGranted() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_GRANTED) {
+                Log.v(TAG,"Permission is granted");
+                return true;
+            } else {
+                Log.v(TAG,"Permission is revoked");
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+                return false;
+            }
+        } else { //permission is automatically granted on sdk<23 upon installation
+            Log.v(TAG,"Permission is granted");
+            return true;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            Log.v(TAG,"Permission: "+permissions[0]+ "was "+grantResults[0]);
+            //resume tasks needing this permission
+            generatePdfMethod();
+        }
+    }
+
+    public void showHUD(){
+        pg.setVisibility(View.VISIBLE);
+        alpView.setVisibility(View.VISIBLE);
+        generatePdfBtn.setEnabled(false);
+        selectTxt.setEnabled(false);
+    }
+
+    public void hideHUD(){
+        pg.setVisibility(View.GONE);
+        alpView.setVisibility(View.GONE);
+        generatePdfBtn.setEnabled(true);
+        selectTxt.setEnabled(true);
+    }
+
 }
